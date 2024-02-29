ea8852c1f17ad7d14ff107a23aae81a196c61c83
GitHub Action <action@github.com>
2024-02-29 08:52:53 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -129,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://harpalgeo.tv/live'),
+                  url: Uri.parse('https://www.youtube2.com/'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
