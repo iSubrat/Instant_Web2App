6aea1f7cfa9217a7875668ca3b6f685f672d1533
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-02-29 18:32:04 +0530

    Update step02.py

--- step02.py
+++ step02.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     database = os.environ['DB_NAME']
 
     # Example query
-    query = "SELECT * FROM app_data WHERE status = 'building'"
+    query = "SELECT * FROM app_data WHERE status = 'building' ORDER BY id DESC LIMIT 1"
 
     # Execute the query
     execute_query(host, username, password, database, query)
