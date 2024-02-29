8c615295f3a5e8a56ccb5ed02f91229c264fffba
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-02-29 16:25:45 +0530

    Update 02_check_status.py

--- 02_check_status.py
+++ 02_check_status.py
@@ -35,7 +35,8 @@ def execute_query(db_host, db_username, db_password, db_database, query):
           connection.commit()
           print("Status column updated to 'started building'")
         else:
-          raise RuntimeError("There is no app for build.")
+          sys.exit(0)
+          # raise RuntimeError("There is no app for build.")
 
         # Close the cursor and connection
         cursor.close()
