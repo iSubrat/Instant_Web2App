85e56a30113f6a66d35b68662f0752f7c170564d
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-02-29 09:10:51 +0530

    Update update_android_manifest.py

--- update_android_manifest.py
+++ update_android_manifest.py
@@ -1,20 +1,20 @@
 import xml.etree.ElementTree as ET
 
-# Path to AndroidManifest.xml
-manifest_path = "android/app/src/main/AndroidManifest.xml"
-
-def update_manifest():
+def update_android_label(manifest_path):
+    # Parse the XML file
     tree = ET.parse(manifest_path)
     root = tree.getroot()
 
-    # Make changes to the XML tree as needed
-    # For example, change the package name
-    package_name = "com.example.newpackage"
-    root.attrib["package"] = package_name
+    # Find the <application> element
+    application_element = root.find(".//application")
 
-    # Write the changes back to the file
-    tree.write(manifest_path)
+    # Update the android:label attribute value
+    if application_element is not None:
+        application_element.set("android:label", "AppName0157")
 
+        # Write the changes back to the file
+        tree.write(manifest_path)
 
 if __name__ == "__main__":
-    update_manifest()
+    manifest_path = "android/app/src/main/AndroidManifest.xml"
+    update_android_label(manifest_path)
