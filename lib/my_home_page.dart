23706daf05fde4e8467fb571005deb35dc73c627
GitHub Action <action@github.com>
2024-03-06 05:25:47 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -129,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://www.dell.com/en-us'),
+                  url: Uri.parse('https://rupureappstore.blogspot.com/'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
