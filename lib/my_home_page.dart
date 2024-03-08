6ac76cb53a9aab485b6ba125ca850f133bc7567c
GitHub Action <action@github.com>
2024-03-08 01:35:41 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -129,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://app.almatinmedia.com/'),
+                  url: Uri.parse('https://fanstube.link'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
