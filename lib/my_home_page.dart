dac86b769e794c83ff0f3edcf5cb692866b7a801
GitHub Action <action@github.com>
2024-03-12 17:24:50 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('htpps://wepoi.github.io/fdstudio/'),
+                  url: Uri.parse('https://www.youtube.com'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
