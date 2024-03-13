bca69d0aa793c054222986257f8dbeaa5968bd18
GitHub Action <action@github.com>
2024-03-13 03:15:44 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://www.youtube.com'),
+                  url: Uri.parse('https://chat.openai.com'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
