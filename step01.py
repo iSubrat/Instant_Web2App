380877ca64eabcffe23b9ca8eeef2364b87ec90d
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-03-13 07:44:02 +0530

    Update step01.py

--- step01.py
+++ step01.py
@@ -92,7 +92,5 @@ if __name__ == "__main__":
   
       # Execute the query
       execute_query(host, username, password, database, query)
-      with open('test.txt', 'w') as file:
-          file.write('this is test content.')
     except Exception as e:
       raise RuntimeError("Process Aborted.")
