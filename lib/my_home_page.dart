ac7c2de0e2ba27861a5202b17b06ff88250361c1
GitHub Action <action@github.com>
2024-03-14 02:04:46 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://openrouter.ai'),
+                  url: Uri.parse('http://lotto123.iwinv.net/lottobible_johapgi33.html'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
