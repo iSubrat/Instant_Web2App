465ca567fa5baeb960156186502b60fa4dbc92d2
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-03-17 10:27:34 +0530

    Update step04.py

--- step04.py
+++ step04.py
@@ -80,7 +80,7 @@ def send_email(sender_email, sender_password, username, recipient_email, subject
             </a>
                 <div style="text-align: left;">
                     <p>Dear {username},<br>Congratulations! Your app {appname} is ready to download. Please click the below button:</p>
-                    <a href="https://appcollection.in/InstantWeb2App/downloads/{appname_link}" class="button"><img src=f"{app_logo_url}" alt="Download" style="width: 25px; height: 25px;"> Download Your App ({appname_link})</a><br>
+                    <a href="https://appcollection.in/InstantWeb2App/downloads/{appname_link}" class="button"><img src="{app_logo_url}" alt="Download" style="width: 25px; height: 25px;"> Download Your App ({appname_link})</a><br>
                     <br>
                     <p>Publish your app on the Play Store for just $50!</p>
                     <a href="https://api.whatsapp.com/send?phone=916397285262&text=Hi%20Developer%2C%20I%20want%20to%20publish%20my%20app%20on%20Google%20Play." class="button button-secondary">Publish Now</a>
