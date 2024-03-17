70a7e7dd31e4509b83195a1eed158e121b550e53
GitHub Action <action@github.com>
2024-03-17 05:11:05 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://www.fastpurse.in/agent'),
+                  url: Uri.parse('https://store.shoopy.in/grocerychouhanstore'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
