f44f59e69a581e4a6212663cd0e3f13953334872
GitHub Action <action@github.com>
2024-03-21 18:10:16 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://unijob.top/'),
+                  url: Uri.parse('https://presensi.museumsubang.com'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
