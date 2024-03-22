227c195d6eab07e49a435ee1e4646aadf958178f
GitHub Action <action@github.com>
2024-03-22 06:53:51 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://rapidsave.com/'),
+                  url: Uri.parse('https://www.icloud.com/'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
