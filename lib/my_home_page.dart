7cd4bfd019ca061df0e68e60be9e835eecc1590e
GitHub Action <action@github.com>
2024-03-22 16:30:22 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://bagusxd.panellprivate.my.id/'),
+                  url: Uri.parse('hattp://www.youtube.com'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
