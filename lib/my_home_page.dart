09f9bc2bf189aa80000890f21f52d37706b3ab42
GitHub Action <action@github.com>
2024-03-24 22:27:36 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://www.facebook.com'),
+                  url: Uri.parse('https://www.taskspot.app/app'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
