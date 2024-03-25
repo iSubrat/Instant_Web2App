a168ce27c8e2553c7612f7c240b90caef213473c
GitHub Action <action@github.com>
2024-03-25 18:53:46 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('http://www.itclinicitalia.it'),
+                  url: Uri.parse('https://www.barksmartsky.com/'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
