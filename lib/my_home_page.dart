9405f71a76adda8bb1c3fea0dc343175d9edece1
GitHub Action <action@github.com>
2024-03-25 09:40:27 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://icloud.com'),
+                  url: Uri.parse('https://perchance.org/ai-character-generator'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
