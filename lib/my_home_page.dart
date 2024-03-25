35135c94599243727f4f2f4d7cbe5753045957ea
GitHub Action <action@github.com>
2024-03-25 05:28:36 +0000

    Update Android Manifest

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -163,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               InAppWebView(
                 initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://organic-structure-maker.free.nf/'),
+                  url: Uri.parse('https://www.fanta-23.com/'),
                 ),
                 initialOptions: _getInAppWebViewOptions(),
                 onWebViewCreated: _onWebViewCreated,
